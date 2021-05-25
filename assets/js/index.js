023f11db2e356238329a415e4eec979b3f803b0c
Brian <semperindoctrina19@gmail.com>
2021-05-25 18:57:20 -0500

    added close mobile nav on select

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var navElems = document.querySelectorAll('.sidenav');
+    var navElems = document.querySelector('.sidenav');
     var carouselElems = document.querySelectorAll('.carousel');
     var FAQelems = document.querySelectorAll('.collapsible');
     var copyright = document.querySelector(".copyright");
     var year = new Date().getFullYear();
-
+    var sidenav = M.Sidenav.init(navElems);
+    
     M.Collapsible.init(FAQelems);
     M.Carousel.init(carouselElems);
-    M.Sidenav.init(navElems);
+
+    for (var child of navElems.children) {
+        child.addEventListener("click", function(){
+            sidenav.close();
+        });
+    }
 
     copyright.innerHTML = "Copyright " + year + " Monroe Chess Club";
 });
